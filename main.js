48596718cb1beecf2c0fd41be8a6ac66ff522562
martin tolaba <juantolaba70@gmail.com>
2024-08-05 23:08:15 -0300

    martin.js

---
+++ main.js
@@ -0,0 +1,50 @@
+let nombre
+let casado = true
+let edad = 33
+
+let apellido = "tolaba"
+
+const url = "www.google.com"
+const pi = 3.14
+
+nombre = "martin"
+
+//console.log(nombre)
+
+nombre = "alan"
+
+//console.log(nombre)
+
+//console.log(casado)
+
+//console.log(edad - 1)
+//console.log(edad * 2)
+//console.log(edad / 1)
+//console.log(edad + 1)
+
+console.log(nombre + " " + apellido)
+
+//alert("bienvenido")
+
+let confirmacion = confirm("¿queres facturar eso?")
+
+console.log(confirmacion)
+
+nombre = prompt("¿cual es tu nombre?")
+
+console.log(nombre)
+
+alert("hola" + nombre + "¿como estas?")
+
+edad = prompt("¿cuantos años tenes?")
+
+edad = parseInt(edad)
+
+alert("dentro de 20 años tendrias: " + (edad +20) )
+
+//aguante Classlist
+nodopadre.innerhtml = <div>
+     <h2> Mantenimientos  </h2>
+    <h2>alineado y balanceo</h2>
+    <h2>servis</h2>
+    <h2>rotacion de cubierta</h2>
